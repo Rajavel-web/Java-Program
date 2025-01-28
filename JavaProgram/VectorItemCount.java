dd6d5b2263524320dfb5c2d67bcdcc962b335f5f
Rajavel-web <rajavelmanivel13@gmail.com>
2025-01-28 09:34:34 +0530

    Add files via upload

---
+++ JavaProgram/VectorItemCount.java
@@ -0,0 +1,20 @@
+package TechTest270725;
+import java.util.Vector;
+
+public class VectorItemCount{
+	
+	    public static void main(String[] args) {
+	    	Vector ve = new Vector();
+	    	ve.add(1);
+	    	ve.add(2);
+	    	ve.add(true);
+	    	ve.add("Rajavel");
+	    	ve.add("Vasanth");
+	    	
+
+	        System.out.println("Size of vector collection: " + ve.size());
+	    }
+	    
+}
+	    
+	     
